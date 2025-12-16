2c01cd2fc4d29d62e02d4b0f483234b101547fc8
alex3mmer <alejandrotrimmer@gmail.com>
2025-12-15 21:40:03 -0600

    Minor improvements

--- temperature_convertor.py
+++ temperature_convertor.py
@@ -5,50 +5,23 @@ usar_convertidor_cf = input("Quieres usar el convertidor de temperatura de C-F? 
 if usar_convertidor_cf == "si":
     print("Empecemos")
     pide_numero = (input("Dame un numero: "))
-    if pide_numero.isdigit():
-        numero = (int(pide_numero))
-        formula = 1.8 * numero + 32
-        resultado = int(formula)
-        print(f"Estamos a {resultado} grados fahrenheit")
+    while pide_numero.isdigit() is False:
+         pide_numero = (input("Me has dado un valor no convertible, dame un numero porfis: "))
+    numero = (int(pide_numero))
+    formula = 1.8 * numero + 32
+    resultado = int(formula)
+    print(f"Estamos a {resultado} grados fahrenheit")
 elif usar_convertidor_cf == "no":
         segunda_convertidor_fc = input("Quieres usar el convertidor de temperatura de F-C? ")
         if segunda_convertidor_fc == "si":
             print("Empecemos")
             pide_numero2 = (input("Dame un numero: "))
-            if pide_numero2.isdigit():
-                numero2 = (int(pide_numero2))
-                formula2 = (numero2 - 32) / 1.8
-                resultado2 = int(formula2)
-                print(f"Estamos a {resultado2} grados celsius")
-        else:
-            print("Ok, adios")
-    
-
-      
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-   
-  
-
-
-
+            while pide_numero2.isdigit() is False:
+                pide_numero2 = (input("Me has dado un valor no convertible, dame un numero porfis: "))
+            numero2 = (int(pide_numero2))
+            formula2 = (numero2 - 32) / 1.8
+            resultado2 = int(formula2)
+            print(f"Estamos a {resultado2} grados celsius")
+else:
+    print("Ok, adios")
+print("Gracisa por usar mi convertidor c:")
